0e06f5b02736c960c7d65d2e9c8eb4cfee0b39ba
Franco Alvarez <alvarez.fr893@gmail.com>
2023-07-15 11:45:40 +0200

    Update kafka consumer config

--- src/main/java/com/kafka/example/notifications/notifications/infra/adapter/in/KafkaConsumerConfig.java
+++ src/main/java/com/kafka/example/notifications/notifications/infra/adapter/in/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import com.kafka.example.events.domain.Event;
 import com.kafka.example.notifications.notifications.infra.port.in.BrokerMessagingConsumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -21,6 +22,9 @@ import java.util.Map;
 public class KafkaConsumerConfig implements BrokerMessagingConsumer<ConsumerFactory<String, Event<?>>, ConcurrentKafkaListenerContainerFactory<String, Event<?>>> {
 
     private final String bootstrapAddress = "localhost:29092";
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String offset;
+
 
     @Bean
     @Override
@@ -33,6 +37,12 @@ public class KafkaConsumerConfig implements BrokerMessagingConsumer<ConsumerFact
 
         props.put(JsonSerializer.TYPE_MAPPINGS,
                 "com.kafka.example:com.kafka.example.events.domain.CustomerCreatedEvent");
+
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group2");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset);
 
 
         final JsonDeserializer<Event<?>> jsonDeserializer = new JsonDeserializer<>();
@@ -50,7 +60,6 @@ public class KafkaConsumerConfig implements BrokerMessagingConsumer<ConsumerFact
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(consumerFactory());
-
         return factory;
     }
 }
